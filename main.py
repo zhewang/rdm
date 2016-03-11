377b18b3bf75f1699ae9d3db8e709710e75a4d7f
Zhe Wang <zwang.contact@gmail.com>
2016-03-10 23:18:10 -0700

    initial simplecolormap

--- main.py
+++ main.py
@@ -10,4 +10,8 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/simplecolormap')
+def simplecolormap():
+    return render_template('simple_color_map.html')
+
 app.run(port=8080, debug=True)
