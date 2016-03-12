d9f682c6db113df06c1f4757c067a5f7a27c4c29
Zhe Wang <zwang.contact@gmail.com>
2016-03-12 09:45:47 -0700

    add a drop down list to select dive level

--- static/js/plot_simplecolormap.js
+++ static/js/plot_simplecolormap.js
@@ -3,9 +3,22 @@ var quadtree_level = 25;
 var variable_schema = ['count', '0', '1', '2', '0*0', '0*1', '0*2', '1*1', '1*2', '2*2'];
 nc.setup(variable_schema);
 
-var diveLevel = 6;
 
-function plot() {
+$(document).ready(function(){
+    var sel = d3.select('#levelChange');
+    sel.selectAll('option').data([0,1,2,3,4,5,6,7,8]).enter()
+    .append('option')
+    .attr('value', function(d){ return d;})
+    .text(function(d){ return d;});
+
+    sel.on('change', function(d){
+        plot(this.value);
+    });
+
+    plot();
+});
+
+function plot(diveLevel=5) {
     var xExtent = [0,10];
     var yExtent = [0,10];
 
@@ -14,6 +27,8 @@ function plot() {
 
     var margin = { top: 30, right: 30, bottom: 30, left: 30 };
 
+    d3.select("#heatmap").selectAll("svg").remove();
+    d3.select("#heatmap").selectAll("rect").remove();
     var svgSel = d3.select("#heatmap")
         .append("svg")
         .attr("width", plotWidth)
@@ -27,12 +42,29 @@ function plot() {
     var gridXSize = Math.floor(contentWidth/Math.pow(2,diveLevel));
     var gridYSize = Math.floor(contentHeight/Math.pow(2,diveLevel));
 
+    // Update the width and height because grid size id converted to integer, so
+    // gridSize*number_of_bins is changed
     contentWidth = gridXSize*Math.pow(2,diveLevel);
     contentHeight = gridYSize*Math.pow(2,diveLevel);
 
     var xScale = d3.scale.linear().domain(xExtent).range([0, contentWidth]);
     var yScale = d3.scale.linear().domain(yExtent).range([contentHeight, 0]);
 
+    // Draw the axis
+    var xAxis = d3.svg.axis().scale(xScale).ticks(10);
+    var yAxis = d3.svg.axis().scale(yScale).ticks(10);
+
+    var svg = d3.select("#heatmap").select('svg');
+    svg.append("g")
+    .attr("transform", "translate("+margin.left+", "+(contentHeight+margin.top).toString()+")")
+    .call(xAxis);
+
+    yAxis.orient("left");
+    svg.append("g")
+    .attr("transform", "translate("+margin.left+", "+margin.top+")")
+    .call(yAxis);
+
+    // Draw the heatmap
     var q = nanocube_server_url+'/count.a("location",dive(tile2d(0,0,0),'+diveLevel+'),"img")'
     nc.query(q, function(d){
         var data = d.root.children;
@@ -68,25 +100,11 @@ function plot() {
                     d3.select(this).classed('selected', false);
                     sel.attr('opacity', '1.0');
                 }
+                console.log(d);
             });
         }
 
     });
-
-
-    var xAxis = d3.svg.axis().scale(xScale).ticks(10);
-    var yAxis = d3.svg.axis().scale(yScale).ticks(10);
-
-    var svg = d3.select("#heatmap").select('svg');
-    svg.append("g")
-    .attr("transform", "translate("+margin.left+", "+(contentHeight+margin.top).toString()+")")
-    .call(xAxis);
-
-    yAxis.orient("left");
-    svg.append("g")
-    .attr("transform", "translate("+margin.left+", "+margin.top+")")
-    .call(yAxis);
-
 }
 
 if (!String.prototype.format) {
