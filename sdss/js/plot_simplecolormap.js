decb0fe190736fa96f16f5960816827bfd515079
Zhe Wang <zwang.contact@gmail.com>
2016-03-15 12:06:09 -0700

    restructure the code

--- sdss/js/plot_simplecolormap.js
+++ sdss/js/plot_simplecolormap.js
@@ -5,6 +5,11 @@ var variable_schema = ['count', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
 nc.setup(variable_schema);
 
 
+///////////////////////////////////////////////////////////////////////////////
+// Common Functions to Setup the Page and Help Plot Heatmaps
+///////////////////////////////////////////////////////////////////////////////
+
+// Change Dive Level
 $(document).ready(function(){
     var sel = d3.select('#levelChange');
     var levels = [];
@@ -25,68 +30,80 @@ $(document).ready(function(){
 
 });
 
+function plot(diveLevel) {
+    d3.select("#heatmap").selectAll("svg").remove();
+    d3.select("#heatmap").selectAll("rect").remove();
 
-function svg2img(svgID){
-    var svg  = document.getElementById(svgID);
-    var xml  = new XMLSerializer().serializeToString(svg);
-    var data = "data:image/svg+xml;base64," + btoa(xml);
-    var img  = new Image();
+    var q = nanocube_server_url+
+            '/count.a("location",dive(tile2d(0,0,0),'+
+            diveLevel+'),"img")';
 
-    img.setAttribute('src', data);
-    document.body.appendChild(img);
+    nc.query(q, function(d){
+        // Plot different kinds of heatmap here
+        plotHeatmap(diveLevel, d, CovMatColorMap);
+        plotHeatmap(diveLevel, d, CovMatColorMap);
+    });
 }
 
-function plot(diveLevel) {
+function plotHeatmap(diveLevel, data,
+                     cellStyleFunc, // How to render each cell
+                     dataTransformFunc=function(d){return d;} // Optional.
+                    )
+{
+    data = data.root.children;
+
     var xExtent = [12,33];
     var yExtent = [10,33];
 
     var plotWidth = 500;
     var plotHeight = 500;
 
-    var margin = { top: 30, right: 30, bottom: 30, left: 30 };
+    var margin = { top: 10, right: 30, bottom: 30, left: 30 };
+    var viewBoxWidth = 500;
+    var viewBoxHeight = 500;
 
-    var contentWidth = plotWidth-margin.left-margin.right;
-    var contentHeight = plotHeight-margin.top-margin.bottom;
-
-    var gridXSize = Math.max(1,Math.floor(contentWidth/Math.pow(2,diveLevel)+0.5));
-    var gridYSize = Math.max(Math.floor(contentHeight/Math.pow(2,diveLevel)+0.5));
-
-    // Update the width and height because grid size id converted to integer, so
-    // gridSize*number_of_bins is changed
-    contentWidth = gridXSize*Math.pow(2,diveLevel);
-    contentHeight = gridYSize*Math.pow(2,diveLevel);
-
-    plotWidth = contentWidth+margin.left+margin.right;
-    plotHeight = contentHeight+margin.top+margin.bottom;
-
-    d3.select("#heatmap").selectAll("svg").remove();
-    d3.select("#heatmap").selectAll("rect").remove();
+    var contentWidth = viewBoxWidth-margin.left-margin.right;
+    var contentHeight = viewBoxHeight-margin.top-margin.bottom;
 
     // plot heatmap
     var svgSel = d3.select("#heatmap")
         .append("svg")
         .attr("width", plotWidth)
         .attr("height", plotHeight)
-        .attr('viewBox', '0 0 '+plotWidth+' '+plotHeight)
-        .attr('id', 'plot')
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top+ ")");
+        .attr('viewBox', '0 0 '+viewBoxWidth+' '+viewBoxHeight)
+        .attr('id', 'plot');
 
     var xScale = d3.scale.linear().domain(xExtent).range([0, contentWidth]);
     var yScale = d3.scale.linear().domain(yExtent).range([contentHeight, 0]);
+
+    var gridXSize = (xScale(xExtent[1])-xScale(xExtent[0]))/Math.pow(2,diveLevel);
+    var gridYSize = (yScale(yExtent[0]-yScale(yExtent[1])))/Math.pow(2,diveLevel);
+
+    svgSel.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top+ ")")
+        .selectAll("rect").data(data).enter()
+        .append("rect")
+        .attr("x", function(d) {
+            return d.x*gridXSize;
+        })
+        .attr("y", function(d) {
+            return contentHeight-(d.y+1)*gridYSize;
+        })
+        .attr("width", gridXSize)
+        .attr("height", gridYSize)
+        .call(cellStyleFunc, data);
 
     // Draw the axis
     var xAxis = d3.svg.axis().scale(xScale).ticks(10);
     var yAxis = d3.svg.axis().scale(yScale).ticks(10);
 
-    var svg = d3.select("#heatmap").select('svg');
-    svg.append("g")
+    svgSel.append("g")
     .attr("transform", "translate("+margin.left+", "+
           (contentHeight+margin.top).toString()+")")
     .call(xAxis);
 
     yAxis.orient("left");
-    svg.append("g")
+    svgSel.append("g")
     .attr("transform", "translate("+margin.left+", "+margin.top+")")
     .call(yAxis);
 
@@ -105,60 +122,47 @@ function plot(diveLevel) {
         .attr('font-size', 10);
     };
 
-    svg.call(setAxisStyle);
+    svgSel.call(setAxisStyle);
+}
 
-    // Draw the heatmap
-    var q = nanocube_server_url+
-            '/count.a("location",dive(tile2d(0,0,0),'+
-            diveLevel+'),"img")';
-
-    nc.query(q, function(d){
-        var data = d.root.children;
-        var countExtent = d3.extent(data, function(d){
-            return d.val.count
-        });
-        countExtent[0] = 0;
-        var opacityScale = d3.scale.linear().domain(countExtent).range([0, 1]);
-
-        var binSel = svgSel.selectAll("rect").data(data).enter()
-        .append("rect")
-        .attr('opacity', function(d){return opacityScale(d.val.count)})
-        .call(setBin);
-
-        //svg2img('plot');
+///////////////////////////////////////////////////////////////////////////////
+// Different Color Mapping Strategies
+///////////////////////////////////////////////////////////////////////////////
+function CovMatColorMap(rectSel, data) {
+    var countExtent = d3.extent(data, function(d){
+        return d.val.count
     });
+    countExtent[0] = 0;
+    var opacityScale = d3.scale.linear().domain(countExtent).range([0, 1]);
 
-    function setBin(sel) {
-        function getColor (d) {
+    rectSel.call(setCellStyle);
+
+    function setCellStyle(sel) {
+        sel.attr("fill", function(d){
             var r = Math.abs(Math.floor(d.val.cov_matrix[0][0]*10));
             var g = Math.abs(Math.floor(d.val.cov_matrix[1][1]*10));
             var b = Math.abs(Math.floor(d.val.cov_matrix[2][2]*10));
             return 'rgb({0},{1},{2})'.format(r,g,b);
-        };
-
-        sel.attr("fill", function(d){return getColor(d)})
-        .attr("x", function(d) {
-            return d.x*gridXSize;
         })
-        .attr("y", function(d) {
-            return contentHeight-(d.y+1)*gridYSize;
-        })
-        .attr("width", gridXSize)
-        .attr("height", gridYSize)
+        .attr('opacity', function(d){return opacityScale(d.val.count)})
         .on("click", function(d) {
-            if(d3.select(this).classed('selected') == false) {
-                sel.attr('opacity', '0.5');
-                d3.select(this)
-                .attr('opacity', '1.0');
-                d3.select(this).classed('selected', true);
-            } else {
-                d3.select(this).classed('selected', false);
-                sel.attr('opacity', '1.0');
-            }
             console.log(d);
+            //if(d3.select(this).classed('selected') == false) {
+                //sel.attr('opacity', '0.5');
+                //d3.select(this)
+                //.attr('opacity', '1.0');
+                //d3.select(this).classed('selected', true);
+            //} else {
+                //d3.select(this).classed('selected', false);
+                //sel.attr('opacity', '1.0');
+            //}
         });
     }
 }
+
+///////////////////////////////////////////////////////////////////////////////
+// Helper Functions
+///////////////////////////////////////////////////////////////////////////////
 
 if (!String.prototype.format) {
   String.prototype.format = function() {
@@ -170,4 +174,14 @@ if (!String.prototype.format) {
       ;
     });
   };
+}
+
+function svg2img(svgID){
+    var svg  = document.getElementById(svgID);
+    var xml  = new XMLSerializer().serializeToString(svg);
+    var data = "data:image/svg+xml;base64," + btoa(xml);
+    var img  = new Image();
+
+    img.setAttribute('src', data);
+    document.body.appendChild(img);
 }
