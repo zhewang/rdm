506688a899761c65a77942d5e7006b812ab67db1
Zhe Wang <zwang.contact@gmail.com>
2016-03-22 12:19:39 -0700

    update color

--- sdss/js/plot_simplecolormap.js
+++ sdss/js/plot_simplecolormap.js
@@ -2,7 +2,7 @@
 // Global variables
 ///////////////////////////////////////////////////////////////////////////////
 
-var nanocube_server_url = 'http://hdc.cs.arizona.edu/nanocube/10020/';
+var nanocube_server_url = 'http://hdc.cs.arizona.edu/nanocube/10040/';
 //var nanocube_server_url = 'http://localhost:29512/';
 var quadtree_level = 15;
 var variable_schema = ['count', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0*0', '0*1', '0*2', '0*3', '0*4', '0*5', '0*6', '0*7', '0*8', '0*9', '1*1', '1*2', '1*3', '1*4', '1*5', '1*6', '1*7', '1*8', '1*9', '2*2', '2*3', '2*4', '2*5', '2*6', '2*7', '2*8', '2*9', '3*3', '3*4', '3*5', '3*6', '3*7', '3*8', '3*9', '4*4', '4*5', '4*6', '4*7', '4*8', '4*9', '5*5', '5*6', '5*7', '5*8', '5*9', '6*6', '6*7', '6*8', '6*9', '7*7', '7*8', '7*9', '8*8', '8*9', '9*9'];
@@ -97,8 +97,8 @@ function CreateHeatmap(cellStyleFunc, dataTransformFunc){
         .attr("y", function(d) {
             return contentHeight-(d.y+1)*gridYSize;
         })
-        .attr("width", gridXSize)
-        .attr("height", gridYSize);
+        .attr("width", ~~gridXSize+1)
+        .attr("height", ~~gridYSize+1);
     }
 
     plot.rectSel = plot.svgSel.append("g")
@@ -284,14 +284,14 @@ function onlyCount(rectSel, data) {
     var countExtent = d3.extent(data, function(d){
         return d.val[0];
     });
-    //countExtent[0] = 0;
+    countExtent[0] = 0;
     var opacityScale = d3.scale.linear()
-        .domain(countExtent).range([0, 1]);
+        .domain(countExtent).range([0.1, 1]);
 
     rectSel.call(setCellStyle);
 
     function setCellStyle(sel) {
-        sel.attr("fill", 'red')
+        sel.attr("fill", '#f03b20')
         .attr('opacity', function(d){return opacityScale(d.val[0])})
         .on('click', function(d){ console.log(d); });
     }
